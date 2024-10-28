0b3a80d2c3ab12f10645faa0e51f67cc4fb44358
Josep Boix Requesens <jspboix@gmail.com>
2024-10-28 11:58:21 +0100

    feat: add user agent parser to enrich event data with browser details
    Resolves #6 by processing incoming JSON data to extract the user agent string from the
    `browser.agent` field and using YAUAA to parse it. Enriches the JSON node with detailed
    browser, device, and OS information.
    
    Changes:
    
    - Added YAUAA library for user agent parsing.
    - Integrated Kotest for testing and wrote unit tests for deserialization.
    
    Co-authored-by: Gaëtan Muller <m.gaetan89@gmail.com>

--- src/main/kotlin/ch/srgssr/pillarbox/monitoring/event/model/EventRequest.kt
+++ src/main/kotlin/ch/srgssr/pillarbox/monitoring/event/model/EventRequest.kt
@@ -2,6 +2,14 @@ package ch.srgssr.pillarbox.monitoring.event.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.node.ObjectNode
+import nl.basjes.parse.useragent.UserAgent
+import nl.basjes.parse.useragent.UserAgentAnalyzer
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.DateFormat
 import org.springframework.data.elasticsearch.annotations.Document
@@ -32,6 +40,77 @@ data class EventRequest(
   @Field(type = FieldType.Date, format = [DateFormat.epoch_millis], name = "@timestamp")
   var timestamp: Long,
   var version: Long,
+  @JsonDeserialize(using = DataDeserializer::class)
   var data: Any? = null,
   var session: Any? = null,
 )
+
+/**
+ * Custom deserializer for the 'data' field in [EventRequest].
+ *
+ * This deserializer processes the incoming JSON data to extract the user agent string from the
+ * `browser.agent` field and enriches the JSON node with detailed information about the browser,
+ * device, and operating system.
+ *
+ * If the `browser.agent` field is not present, the deserializer returns the node unmodified.
+ */
+private class DataDeserializer : JsonDeserializer<JsonNode?>() {
+  companion object {
+    private val userAgentAnalyzer =
+      UserAgentAnalyzer
+        .newBuilder()
+        .hideMatcherLoadStats()
+        .withCache(10000)
+        .build()
+  }
+
+  override fun deserialize(
+    parser: JsonParser,
+    ctxt: DeserializationContext,
+  ): JsonNode {
+    val node: JsonNode = parser.codec.readTree(parser)
+    val browserNode = (node as? ObjectNode)?.get("browser")
+    val userAgent =
+      (browserNode as? ObjectNode)
+        ?.get("agent")
+        ?.asText()
+        ?.let(userAgentAnalyzer::parse) ?: return node
+
+    node.set<ObjectNode>(
+      "browser",
+      browserNode.apply {
+        put("name", userAgent.getValueOrNull("AgentName"))
+        put("version", userAgent.getValueOrNull("AgentVersion"))
+      },
+    )
+
+    node.set<ObjectNode>(
+      "device",
+      ObjectNode(ctxt.nodeFactory).apply {
+        put("name", userAgent.getValueOrNull("DeviceName"))
+        put("version", userAgent.getValueOrNull("DeviceVersion"))
+      },
+    )
+
+    node.set<ObjectNode>(
+      "os",
+      ObjectNode(ctxt.nodeFactory).apply {
+        put("name", userAgent.getValueOrNull("OperatingSystemName"))
+        put("version", userAgent.getValueOrNull("OperatingSystemVersion"))
+      },
+    )
+
+    return node
+  }
+}
+
+/**
+ * Private extension function for [UserAgent] to return `null` instead of "??" for unknown values.
+ *
+ * @param fieldName The name of the field to retrieve.
+ * @return The value of the field, or `null` if the value is "??".
+ */
+private fun UserAgent.getValueOrNull(fieldName: String): String? {
+  val value = this.getValue(fieldName)
+  return if (value == "??") null else value
+}
