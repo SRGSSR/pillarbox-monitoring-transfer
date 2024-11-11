c15357acdf88e6779690e04bb9d14e0ab03532e5
Josep Boix Requesens <jspboix@gmail.com>
2024-11-11 17:12:30 +0100

    feat: index user ip field and define required properties
    Resolves #17 by parsing and indexing the ip property generated by the event dispatcher.
    
    - Added `user_ip` as an indexed field.
    - Added required fields: indexing will fail if any of the required fields is missing.
    
    Co-authored-by: Gaëtan Muller <m.gaetan89@gmail.com>

--- src/main/kotlin/ch/srgssr/pillarbox/monitoring/event/model/EventRequest.kt
+++ src/main/kotlin/ch/srgssr/pillarbox/monitoring/event/model/EventRequest.kt
@@ -23,6 +23,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType
  * @property sessionId The ID of the session associated with the event.
  * @property eventName The name of the event.
  * @property timestamp The timestamp of the event in epoch milliseconds.
+ * @property ip The ip address of the client that generated the event.
  * @property data Additional data associated with the event.
  * @property session Session data associated with the event, potentially updated later.
  */
@@ -31,18 +32,24 @@ data class EventRequest(
   @Id
   @JsonIgnore
   var id: String? = null,
-  @JsonProperty("session_id")
+  @JsonProperty("session_id", required = true)
   @Field("session_id")
   var sessionId: String,
-  @JsonProperty("event_name")
+  @JsonProperty("event_name", required = true)
   @Field("event_name")
   var eventName: String,
   @Field(type = FieldType.Date, format = [DateFormat.epoch_millis], name = "@timestamp")
+  @JsonProperty(required = true)
   var timestamp: Long,
+  @JsonProperty("user_ip")
+  @Field("user_ip")
+  var ip: String?,
+  @JsonProperty(required = true)
   var version: Long,
   @JsonDeserialize(using = DataDeserializer::class)
-  var data: Any? = null,
-  var session: Any? = null,
+  @JsonProperty(required = true)
+  var data: Any,
+  var session: Any?,
 )
 
 /**
