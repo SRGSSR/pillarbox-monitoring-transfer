9bf82b37be974b9d322e47719bf608186979fb3b
Josep Boix Requesens <jspboix@gmail.com>
2026-08-14 21:40:32 +0200

    fix(release): use default relese note generator preset
    - Remove custom conventional commit preset, the latest
      version is producing an empty release note.
    - Replace `npm install -g` from the release workflow
      with a private package.json and lock file.
    - Moved osctl to a standalone package.

--- scripts/osctl/lib/logging.sh
+++
@@ -1,5 +0,0 @@
-# Logging helpers
-success() { echo -e "$(tput setaf 2)✔ $*$(tput sgr0)"; }
-error()   { echo -e "$(tput setaf 1)✖ $*$(tput sgr0)"; }
-info()    { echo -e "$(tput setaf 4)ℹ $*$(tput sgr0)"; }
-warn()    { echo -e "$(tput setaf 3)! $*$(tput sgr0)"; }
